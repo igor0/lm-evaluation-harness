e0c19389ba9e20100619a5a1f6b4c08aaa928851
Igor Ostrovsky <igoros@gmail.com>
2021-12-27 07:29:54 +0000

    The Pile - huggingface

--- lm_eval/tasks/pile.py
+++ lm_eval/tasks/pile.py
@@ -7,39 +7,19 @@ from lm_eval.base import rf, PerplexityTask
 from ..metrics import mean, matthews_corrcoef, f1_score
 from ..utils import general_detokenize
 from best_download import download_file
+from .common import HFTask
 
 
-class PilePerplexityTask(PerplexityTask, abc.ABC):
+class PilePerplexityTask(PerplexityTask, HFTask):
     VERSION = 0
-
-    PILE_SET_NAME = None
-    VAL_PATH = 'data/pile/val.jsonl.zst'
-    TEST_PATH = 'data/pile/test.jsonl.zst'
+    DATASET_PATH = "the_pile"
 
     def download(self):
-        # TODO: separate pile val/test out by component so we don't have to scan the entire file once per set
-        os.makedirs("data/pile/", exist_ok=True)
-        download_file("https://the-eye.eu/public/AI/pile/val.jsonl.zst", self.VAL_PATH, "264c875d8bbd355d8daa9d032b75fd8fb91606218bb84dd1155b203fcd5fab92")
-        download_file("https://the-eye.eu/public/AI/pile/test.jsonl.zst", self.TEST_PATH, "0bb28c52d0b5596d389bf179ce2d43bf7f7ffae76b0d2d20b180c97f62e0975e")
-
-    def validation_docs(self):
-        rdr = lm_dataformat.Reader(self.VAL_PATH)
-        for doc, metadata in rdr.stream_data(get_meta=True):
-            if metadata["pile_set_name"] == self.PILE_SET_NAME:
-                yield doc
-
-    def test_docs(self):
-        rdr = lm_dataformat.Reader(self.TEST_PATH)
-        for doc, metadata in rdr.stream_data(get_meta=True):
-            if metadata["pile_set_name"] == self.PILE_SET_NAME:
-                yield doc
+        super().download()
+        self.data["validation"] = [x["text"] for x in self.data["train"]]
 
     def has_validation_docs(self):
         return True
-
-    def has_test_docs(self):
-        return True
-
 
 class PileArxiv(PilePerplexityTask):
     PILE_SET_NAME = "ArXiv"
@@ -58,15 +38,17 @@ class PileDmMathematics(PilePerplexityTask):
 
 
 class PileEnron(PilePerplexityTask):
-    PILE_SET_NAME = "Enron Emails"
+    DATASET_NAME = "enron_emails"
 
 
 class PileEuroparl(PilePerplexityTask):
     PILE_SET_NAME = "EuroParl"
+    DATASET_NAME = "europarl"
 
 
 class PileFreeLaw(PilePerplexityTask):
     PILE_SET_NAME = "FreeLaw"
+    DATASET_NAME = "free_law"
 
 
 class PileGithub(PilePerplexityTask):
@@ -79,10 +61,12 @@ class PileGutenberg(PilePerplexityTask):
 
 class PileHackernews(PilePerplexityTask):
     PILE_SET_NAME = "HackerNews"
+    DATASET_NAME = "hacker_news"
 
 
 class PileNIHExporter(PilePerplexityTask):
     PILE_SET_NAME = "NIH ExPorter"
+    DATASET_NAME = "nih_exporter"
 
 
 class PileOpenSubtitles(PilePerplexityTask):
@@ -103,10 +87,12 @@ class PilePileCc(PilePerplexityTask):
 
 class PilePubmedAbstracts(PilePerplexityTask):
     PILE_SET_NAME = "PubMed Abstracts"
+    DATASET_NAME = "pubmed"
 
 
 class PilePubmedCentral(PilePerplexityTask):
     PILE_SET_NAME = "PubMed Central"
+    DATASET_NAME = "pubmed_central"
 
 
 class PileStackExchange(PilePerplexityTask):
@@ -115,6 +101,7 @@ class PileStackExchange(PilePerplexityTask):
 
 class PileUspto(PilePerplexityTask):
     PILE_SET_NAME = "USPTO Backgrounds"
+    DATASET_NAME = "uspto"
 
 
 class PileUbuntuIrc(PilePerplexityTask):
